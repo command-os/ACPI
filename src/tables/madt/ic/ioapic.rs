3e444e2465becceaa28129c49995ee22ef3f1ab9
Visual <30368284+VisualDevelopment@users.noreply.github.com>
2022-03-15 15:40:42 +0200

    Add I/O APIC register reading

--- src/tables/madt/ic/ioapic.rs
+++ src/tables/madt/ic/ioapic.rs
@@ -1,6 +1,8 @@
 //! Copyright (c) VisualDevelopment 2021-2022.
 //! This project is licensed by the Creative Commons Attribution-NoCommercial-NoDerivatives licence.
 
+use modular_bitfield::prelude::*;
+
 #[repr(C, packed)]
 #[derive(Debug, Clone, Copy)]
 pub struct IoApic {
@@ -16,6 +18,77 @@ impl core::ops::Deref for IoApic {
 
     fn deref(&self) -> &Self::Target {
         &self.header
+    }
+}
+
+#[repr(u32)]
+#[derive(Debug)]
+pub enum IoApicRegister {
+    IoApicId,
+    IoApicVer,
+    IoApicArbId,
+    IoRedirTbl = 0x10,
+}
+
+#[derive(Debug, BitfieldSpecifier)]
+#[repr(u8)]
+#[bits = 3]
+pub enum DeliveryMode {
+    Fixed,
+    LowestPriority,
+    SMI,
+    NMI = 4,
+    INIT,
+    ExtINT,
+}
+
+#[bitfield(bits = 64)]
+#[derive(Debug)]
+pub struct IoApicRedirect {
+    pub vector: u8,
+    pub delivery_mode: DeliveryMode,
+    pub logical_dest: bool,
+    pub pending: bool,
+    pub active_high: bool,
+    pub remote_irr: bool,
+    pub trigger_at_level: bool,
+    pub masked: bool,
+    #[skip]
+    __: B40,
+    pub dest: B7,
+}
+
+impl IoApic {
+    fn base(&self, off: u8) -> *mut u32 {
+        (self.address as usize + off as usize + amd64::paging::PHYS_VIRT_OFFSET) as *mut u32
+    }
+
+    pub fn read(&self, reg: u32) -> u32 {
+        unsafe {
+            self.base(0).write_volatile(reg);
+            self.base(0x10).read_volatile()
+        }
+    }
+
+    pub fn write(&self, reg: u32, val: u32) {
+        unsafe {
+            self.base(0).write_volatile(reg);
+            self.base(0x10).write_volatile(val);
+        }
+    }
+
+    pub fn read_redir(&self, num: u32) -> IoApicRedirect {
+        let base = IoApicRegister::IoRedirTbl as u32 + num * 2;
+        IoApicRedirect::from_bytes(
+            [
+                self.read(base).to_le_bytes(),
+                self.read(base + 1).to_le_bytes(),
+            ]
+            .concat()
+            .as_slice()
+            .try_into()
+            .unwrap(),
+        )
     }
 }
 
