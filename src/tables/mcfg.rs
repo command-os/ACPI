c6b5dba6f930ed40150a2bc3285405f9e6f94cf3
VisualDevelopment <30368284+VisualDevelopment@users.noreply.github.com>
2022-06-16 00:58:00 +0300

    Fix MCFG table

--- src/tables/mcfg.rs
+++ src/tables/mcfg.rs
@@ -10,11 +10,19 @@ pub struct MCFG {
     __: u64,
 }
 
+impl core::ops::Deref for MCFG {
+    type Target = super::SDTHeader;
+
+    fn deref(&self) -> &Self::Target {
+        &self.header
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 #[repr(C, packed)]
 pub struct MCFGEntry {
     pub base: u64,
-    pub segment: u32,
+    pub segment: u16,
     pub bus_start: u8,
     pub bus_end: u8,
     __: u32,
@@ -24,16 +32,8 @@ impl MCFG {
     pub fn entries(&self) -> &'static [MCFGEntry] {
         let len = (self.length() - size_of::<Self>()) / size_of::<MCFGEntry>();
         unsafe {
-            let data = (self as *const _ as *const u8).add(size_of::<Self>()) as *const MCFGEntry;
+            let data = ((self as *const _ as usize) + size_of::<Self>()) as *const MCFGEntry;
             core::slice::from_raw_parts(data, len)
         }
-    }
-}
-
-impl core::ops::Deref for MCFG {
-    type Target = super::SDTHeader;
-
-    fn deref(&self) -> &Self::Target {
-        &self.header
     }
 }
